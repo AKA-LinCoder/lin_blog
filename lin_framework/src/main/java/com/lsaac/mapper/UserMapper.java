f15b89d9532e8feec9ca91e46b74f4c16413b9a5
lin <2423860442@qq.com>
2022-09-19 15:26:58 +0800

    实现登陆接口准备工作

---
+++ lin_framework/src/main/java/com/lsaac/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.lsaac.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lsaac.domain.entity.User;
+
+
+/**
+ * 用户表(User)表数据库访问层
+ *
+ * @author makejava
+ * @since 2022-09-19 15:16:15
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
