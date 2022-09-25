3421f4ccd03aa0ef06a5967d6a75a3cb540059b1
lin <2423860442@qq.com>
2022-09-25 13:37:46 +0800

    给实体类添加文档注释信息

--- lin_blogs/src/main/java/com/lsaac/controller/CommentController.java
+++ lin_blogs/src/main/java/com/lsaac/controller/CommentController.java
@@ -3,8 +3,10 @@ package com.lsaac.controller;
 import com.lsaac.annotation.SystemLog;
 import com.lsaac.constants.SystemConstants;
 import com.lsaac.domain.ResponseResult;
+import com.lsaac.domain.dto.AddCommentDto;
 import com.lsaac.domain.entity.Comment;
 import com.lsaac.service.CommentService;
+import com.lsaac.utils.BeanCopyUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
@@ -32,7 +34,8 @@ public class CommentController {
 //    @RequestHeader((value = "token"),required = true)String token
     @PostMapping
     @SystemLog(businessName = "添加评论")
-    public ResponseResult comment(@RequestHeader(value = "token",required = true) String token, @RequestBody Comment comment){
+    public ResponseResult comment(@RequestHeader(value = "token",required = true) String token, @RequestBody AddCommentDto addCommentDto){
+        Comment comment = BeanCopyUtils.copyBean(addCommentDto, Comment.class);
         return commentService.addComment(comment);
     }
 
@@ -44,7 +47,7 @@ public class CommentController {
             @ApiImplicitParam(name = "pageSize",value = "每页大小")
     }
     )
-    @SystemLog(businessName = "添加友链评论列表")
+    @SystemLog(businessName = "友链评论列表")
     public ResponseResult linkCommentList(Integer pageNum,Integer pageSize){
         return commentService.commentList(SystemConstants.LINK_COMMENT,null,pageNum,pageSize);
     }
